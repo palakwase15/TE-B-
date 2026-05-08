98b0f15ae5be33b42cc96f49ea2e86aa7ad8b783
Palak Wase <wasepalak@gmail.com>
2026-05-08 19:01:45 +0530

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,12 +1,7 @@
-public class Pattern1 {
-    public static void main(String[] args) {
+# Pattern 1
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end=" ")
+    print()
 
-        for(int i = 1; i <= 5; i++) {
-            for(int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-    }
-}
+print()
